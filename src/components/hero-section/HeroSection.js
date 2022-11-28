1cb66cf12ec1fadbf3e0e1845aa10c7a1b297d99
Michael Campbell <michael.campbell14@gmail.com>
2022-11-28 16:40:09 -0500

    CHORE adding another font for the game title. exported hero img from PS and inserted into bg. styled some text. just button to do

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -1,26 +1,57 @@
 import styled from 'styled-components';
 
+import bgImage from './../../assets/imgs/hero-image.png';
+
 function HeroSection() {
   return (
-    // TODO: put in main bg img
     // TODO: reference valorant website for button design using ::before and ::after sudo selectors
-    <div>
+    <BigImage>
       <MiddleText>
-        <p>Lorem Ipsum</p>
+        <p>A 5v5 character-based tactical shooter</p>
         <h1>SKULL SPLIT</h1>
         <MainButt>Play Now</MainButt>
       </MiddleText>
-    </div>
+    </BigImage>
   );
 }
+
+const BigImage = styled.div`
+  background-image: url(${bgImage});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: calc(100vh - 100px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const MiddleText = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  color: #ffffff;
+
+  p {
+    font-size: 35px;
+    line-height: normal;
+  }
+
+  h1 {
+    font-family: 'Bebas Neue', cursive;
+    font-size: 200px;
+    line-height: normal;
+  }
 `;
 
-const MainButt = styled.button``;
+const MainButt = styled.button`
+  &::before {
+  }
+
+  &::after {
+  }
+`;
 
 export default HeroSection;
