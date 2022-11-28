8732e17b4c4dfecf90a3adbe5062859786faf5fb
Michael Campbell <michael.campbell14@gmail.com>
2022-11-28 17:41:09 -0500

    CHORE adding the current button style and hover

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -9,7 +9,12 @@ function HeroSection() {
       <MiddleText>
         <p>A 5v5 character-based tactical shooter</p>
         <h1>SKULL SPLIT</h1>
-        <MainButt>Play Now</MainButt>
+        <MainButt>
+          <ButtDiv>
+            <ButtOverlay></ButtOverlay>
+            <p>Play Now</p>
+          </ButtDiv>
+        </MainButt>
       </MiddleText>
     </BigImage>
   );
@@ -46,12 +51,72 @@ const MiddleText = styled.div`
   }
 `;
 
+const buttWidth = 250;
 const MainButt = styled.button`
+  position: relative;
+  width: auto;
+  min-width: ${buttWidth}px;
+  background: none;
+  border: 0;
+  font-size: 35px;
+  padding: 10px;
+
   &::before {
+    content: '';
+    position: absolute;
+    display: block;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 25px;
+    border: 1px solid #ffffff;
+    border-bottom: 0;
   }
 
   &::after {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    height: 25px;
+    border: 1px solid #ffffff;
+    border-top: 0;
   }
+
+  &:hover span {
+    transition: transform 0.5s ease-in-out;
+    transform: translateX(-10%) skew(-10deg);
+  }
+
+  &:hover p {
+    position: relative;
+    z-index: 2;
+    transition: color 1s;
+    color: #ffffff;
+  }
+`;
+
+const ButtDiv = styled.div`
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+  text-align: center;
+  background-color: #f0ff00;
+`;
+
+const ButtOverlay = styled.span`
+  content: '';
+  z-index: 1;
+  position: absolute;
+  display: block;
+  height: 100%;
+  width: 120%;
+  top: 0;
+  background-color: #3b4339;
+  cursor: pointer;
+  transform: translateX(-110%) skew(-10deg);
 `;
 
 export default HeroSection;
