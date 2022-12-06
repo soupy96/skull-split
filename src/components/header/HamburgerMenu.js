ac4997c47b8edd0ac698ef05ace0a57245b4ccfa
Michael Campbell <michael.campbell14@gmail.com>
2022-12-06 15:53:28 -0500

    CHORE trying to get the menu to close when a menu item is clicked

--- src/components/header/HamburgerMenu.js
+++ src/components/header/HamburgerMenu.js
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { stack as Menu } from 'react-burger-menu';
+import { useState } from 'react';
 
 function HamburgerMenu() {
+  // TODO: fix issue where menu closes on first menu item click
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuOpenHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <HamburgerMenuBox>
-      <Menu right noOverlay>
-        <Link to='/'>HOME</Link>
-        <Link to='/about'>ABOUT</Link>
-        <Link to='/contact'>CONTACT</Link>
-        <a href='#'>PLAY NOW</a>
+      <Menu right noOverlay isOpen={isMenuOpen}>
+        <Link to='/' onClick={menuOpenHandler}>
+          HOME
+        </Link>
+        <Link to='/about' onClick={menuOpenHandler}>
+          ABOUT
+        </Link>
+        <Link to='/contact' onClick={menuOpenHandler}>
+          CONTACT
+        </Link>
+        <a href='#' onClick={menuOpenHandler}>
+          PLAY NOW
+        </a>
       </Menu>
     </HamburgerMenuBox>
   );
