0dbb0bcd1f1c4b94f5b492cee8220eef6bc7b500
Michael Campbell <michael.campbell14@gmail.com>
2022-12-06 17:21:16 -0500

    BUG fixing

--- src/components/header/HamburgerMenu.js
+++ src/components/header/HamburgerMenu.js
@@ -31,7 +31,7 @@ function HamburgerMenu() {
         <Link to='/'>HOME</Link>
         <Link to='/about'>ABOUT</Link>
         <Link to='/contact'>CONTACT</Link>
-        <a href='#'>PLAY NOW</a>
+        <Link to='/'>PLAY NOW</Link>
       </Menu>
     </HamburgerMenuBox>
   );
