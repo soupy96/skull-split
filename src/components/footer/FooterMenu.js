5f849df52c499cb78b9a0441cfb5e69d444fa203
Michael Campbell <michael.campbell14@gmail.com>
2022-12-09 17:41:16 -0500

    BUG figure out why when a link is clicked it doesnt go to the top of the page

--- src/components/footer/FooterMenu.js
+++ src/components/footer/FooterMenu.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 function FooterMenu() {
   return (
+    // TODO: figure out why the page doesnt go to the top when i click a link
     <FooterNav>
       <Link to='/privacy-notice'>Privacy Notice</Link>
       <Link to='/terms-of-service'>Terms of Service</Link>
