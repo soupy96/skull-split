02adfed9d0c8859b30c3eb790851d3cc54884480
Michael Campbell <michael.campbell14@gmail.com>
2022-12-09 15:54:15 -0500

    CHORE installing react-helmet for custom title names per page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet';
+
 import HeroSection from './../components/hero-section/HeroSection';
 import GameInfo from './../components/game-info/GameInfo';
 import InfoCards from './../components/info-cards/InfoCards';
@@ -6,6 +8,9 @@ import Updates from './../components/updates/Updates';
 function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Skull Split | Home</title>
+      </Helmet>
       <HeroSection />
       <GameInfo />
       <InfoCards />
