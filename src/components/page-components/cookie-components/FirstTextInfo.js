fe52592a21e512598dba784882903c46679ccf88
Michael Campbell <michael.campbell14@gmail.com>
2022-12-09 20:27:54 -0500

    CHORE cleaning up the cookies pref page

---
+++ src/components/page-components/cookie-components/FirstTextInfo.js
@@ -0,0 +1,44 @@
+function FirstTextInfo() {
+  return (
+    <div>
+      <p>
+        <b>Vivamus sed ipsum euismod, hendrerit nisi non, eleifend erat.</b>
+      </p>
+      <p>
+        <b>Nulla facilisi.</b>
+      </p>
+      <p>
+        Donec tempor quis mauris dictum porta. Nulla auctor gravida egestas.
+        Pellentesque ultricies ipsum in rutrum semper. Aliquam mi eros,
+        tristique et mauris eget, placerat fermentum ex. Sed bibendum nisi in
+        nibh accumsan, et condimentum orci auctor. In tristique elit id neque
+        porttitor venenatis. Mauris ultrices dui ac hendrerit tempus. Aliquam
+        sed pulvinar tellus. Sed aliquet interdum arcu non ullamcorper. Nulla
+        non sodales leo. Mauris quis venenatis odio. Vestibulum condimentum
+        justo et urna volutpat tincidunt. Sed placerat arcu ut risus congue,
+        sagittis porta turpis commodo. Phasellus quis tristique mauris.
+        Suspendisse lobortis auctor diam ornare commodo. Quisque quis ante
+        condimentum, mollis lectus ac, luctus odio.
+      </p>
+      <p>Etiam vehicula dui quis ornare sagittis:</p>
+      <ul>
+        <li>
+          Maecenas dui augue: porttitor a fermentum ac, pharetra vel risus.
+          Aenean semper, lacus eu laoreet venenatis, est tortor pretium leo, id
+          consectetur nisl est ac ante. Suspendisse consequat magna ante, a
+          malesuada dolor rutrum a. Duis rhoncus vitae tellus at tempus.{' '}
+        </li>
+        <li>
+          Vestibulum ornare: diam viverra pulvinar vulputate, risus libero
+          hendrerit tellus, quis tincidunt dolor quam nec odio. Proin aliquet
+          leo fringilla metus sagittis imperdiet. Curabitur in ex et nisl luctus
+          ultrices at at libero. Fusce quis accumsan lacus. Aenean placerat
+          accumsan magna, ut viverra est interdum vel.{' '}
+        </li>
+      </ul>
+      <hr />
+    </div>
+  );
+}
+
+export default FirstTextInfo;
