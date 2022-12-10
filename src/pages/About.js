84b80d035e55aeb7415a763cabbf2a8dd8b1fd36
Michael Campbell <michael.campbell14@gmail.com>
2022-12-10 13:27:45 -0500

    CHORE putting about and contact back into progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,9 @@
+import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 
-import PageTitle from '../components/re-usable/PageTitle';
-import CompanyBuilding from '../components/page-components/about-components/CompanyBuilding';
-import CompanyLeads from '../components/page-components/about-components/CompanyLeads';
-import CompanyCulture from '../components/page-components/about-components/CompanyCulture';
-import Careers from '../components/page-components/about-components/Careers';
-
 import { NestedRootOther } from '../components/re-usable/NestedRoot';
+import PageTitle from '../components/re-usable/PageTitle';
+
 import { BodyText } from '../components/re-usable/styles/LegalStyles';
 
 function About() {
@@ -17,25 +14,15 @@ function About() {
       </Helmet>
       <PageTitle>About</PageTitle>
       <BodyText>
-        <CompanyBuilding />
-        <br />
-        <br />
-        <br />
-        <br />
-        <CompanyLeads />
-        <br />
-        <br />
-        <br />
-        <br />
-        <CompanyCulture />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Careers />
+        <Text>About page in progress</Text>
       </BodyText>
     </NestedRootOther>
   );
 }
+
+const Text = styled.p`
+  text-align: center;
+  margin: 25% auto !important;
+`;
 
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,9 @@
+import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 
-import PageTitle from '../components/re-usable/PageTitle';
-import ContactSection from '../components/page-components/contact-components/ContactSection';
-
 import { NestedRootOther } from '../components/re-usable/NestedRoot';
+import PageTitle from '../components/re-usable/PageTitle';
+
 import { BodyText } from '../components/re-usable/styles/LegalStyles';
 
 function Contact() {
@@ -14,10 +14,15 @@ function Contact() {
       </Helmet>
       <PageTitle>Contact</PageTitle>
       <BodyText>
-        <ContactSection />
+        <Text>Contact page is in progress</Text>
       </BodyText>
     </NestedRootOther>
   );
 }
+
+const Text = styled.p`
+  text-align: center;
+  margin: 25% auto !important;
+`;
 
 export default Contact;
