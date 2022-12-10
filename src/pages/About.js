46a3fab6edb42dbfe5627c8fed26eb3830f023e0
Michael Campbell <michael.campbell14@gmail.com>
2022-12-10 18:21:24 -0500

    CHORE test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,17 @@
 import { Helmet } from 'react-helmet';
 
-import Careers from '../components/page-components/about-components/Careers';
-import CompanyCulture from '../components/page-components/about-components/CompanyCulture';
-import CompanyLeads from '../components/page-components/about-components/CompanyLeads';
-import CompanyBuilding from '../components/page-components/about-components/CompanyBuilding';
 import PageTitle from '../components/re-usable/PageTitle';
+import CompanyBuilding from '../components/page-components/about-components/CompanyBuilding';
+import CompanyLeads from '../components/page-components/about-components/CompanyLeads';
+import CompanyCulture from '../components/page-components/about-components/CompanyCulture';
+import Careers from '../components/page-components/about-components/Careers';
 
 import { NestedRootOther } from '../components/re-usable/NestedRoot';
 
 function About() {
   return (
     // TODO: replace break tags with a margin to better space stuff out
+    // TOOD: make mobile friendly
     <NestedRootOther>
       <Helmet>
         <title>Skull Split | About</title>
