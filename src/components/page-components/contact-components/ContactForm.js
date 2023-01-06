27ba32aed9897ab8cf2f8135437608c1478a9f85
Michael Campbell <michael.campbell14@gmail.com>
2023-01-06 13:36:49 -0500

    CHORE adding a fetch post request to submit to postman and then adding a success msg

--- src/components/page-components/contact-components/ContactForm.js
+++ src/components/page-components/contact-components/ContactForm.js
@@ -1,9 +1,10 @@
-import { ContactFormBox } from '../../re-usable/styles/ContentStyles';
-
+import { useState } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+
+import { ContactFormBox } from '../../re-usable/styles/ContentStyles';
 
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
@@ -24,6 +25,8 @@ const validationSchema = yup.object({
 });
 
 function ContactForm() {
+  const [isSent, setIsSent] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       fullName: '',
@@ -33,59 +36,75 @@ function ContactForm() {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      // setSubmitting(true);
+      console.log(values);
+      // alert(JSON.stringify(values, null, 2));
+      fetch('https://0cec9107-5bdb-4206-a502-4628165230c0.mock.pstmn.io', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      }).then(() => {
+        setIsSent(true);
+        console.log('submitted');
+      });
     },
   });
   return (
     <ContactFormBox>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id='fullName'
-          name='fullName'
-          label='Full Name'
-          value={formik.values.fullName}
-          onChange={formik.handleChange}
-          error={formik.touched.fullName && Boolean(formik.errors.fullName)}
-          helperText={formik.touched.fullName && formik.errors.fullName}
-        />
-        <TextField
-          fullWidth
-          id='email'
-          name='email'
-          label='Email'
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id='phone'
-          name='phone'
-          label='Phone'
-          value={formik.values.phone}
-          onChange={formik.handleChange}
-          error={formik.touched.phone && Boolean(formik.errors.phone)}
-          helperText={formik.touched.phone && formik.errors.phone}
-        />
-        <TextField
-          fullWidth
-          id='message'
-          name='message'
-          label='Message'
-          multiline
-          rows={4}
-          value={formik.values.message}
-          onChange={formik.handleChange}
-          error={formik.touched.message && Boolean(formik.errors.message)}
-          helperText={formik.touched.message && formik.errors.message}
-          minRows={3}
-        />
-        <Button color='primary' variant='contained' fullWidth type='submit'>
-          Submit
-        </Button>
-      </form>
+      {!isSent ? (
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            id='fullName'
+            name='fullName'
+            label='Full Name'
+            value={formik.values.fullName}
+            onChange={formik.handleChange}
+            error={formik.touched.fullName && Boolean(formik.errors.fullName)}
+            helperText={formik.touched.fullName && formik.errors.fullName}
+          />
+          <TextField
+            fullWidth
+            id='email'
+            name='email'
+            label='Email'
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            fullWidth
+            id='phone'
+            name='phone'
+            label='Phone'
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            error={formik.touched.phone && Boolean(formik.errors.phone)}
+            helperText={formik.touched.phone && formik.errors.phone}
+          />
+          <TextField
+            fullWidth
+            id='message'
+            name='message'
+            label='Message'
+            multiline
+            rows={4}
+            value={formik.values.message}
+            onChange={formik.handleChange}
+            error={formik.touched.message && Boolean(formik.errors.message)}
+            helperText={formik.touched.message && formik.errors.message}
+            minRows={3}
+          />
+          <Button color='primary' variant='contained' fullWidth type='submit'>
+            Submit
+          </Button>
+        </form>
+      ) : (
+        <p style={{ textAlign: 'center' }}>Success</p>
+      )}
     </ContactFormBox>
   );
 }
